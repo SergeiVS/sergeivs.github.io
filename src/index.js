994cfdd3fb92e8c100daf8c96e390a1a74c300d8
Сергей Шекшуев <danger_x@MacBook-Pro-Sergej.local>
2018-12-28 14:33:50 +0300

    gifts added

--- src/index.js
+++ src/index.js
@@ -3,29 +3,38 @@
     const screenHeight = 720;
     const rowsCount = 10;
     
+    const imagesFolder = "src/assets/images/";
     
     let mainState = 
     {
         preload: function()
         {
-            game.load.image("santa", "src/assets/santa.png");
-            game.load.image("obstacle", "src/assets/plane.png");
-            game.load.image("sky", "src/assets/sky.png");
-            game.load.image("city", "src/assets/city.png");
-            game.load.image("cloud1", "src/assets/cloud_1.png");
-            game.load.image("cloud2", "src/assets/cloud_2.png");
-            game.load.image("cloud3", "src/assets/cloud_3.png");
-            game.load.image("cloud4", "src/assets/cloud_4.png");
-            game.load.image("cloud5", "src/assets/cloud_5.png");
-            game.load.image("cloud6", "src/assets/cloud_6.png");
-            game.load.image("cloud7", "src/assets/cloud_7.png");
-            game.load.image("cloud8", "src/assets/cloud_8.png");
+            game.load.image("santa", imagesFolder + "santa.png");
+            game.load.image("obstacle", imagesFolder + "plane.png");
+            game.load.image("sky", imagesFolder + "sky.png");
+            game.load.image("city", imagesFolder + "city.png");
+            game.load.image("cloud1", imagesFolder + "cloud_1.png");
+            game.load.image("cloud2", imagesFolder + "cloud_2.png");
+            game.load.image("cloud3", imagesFolder + "cloud_3.png");
+            game.load.image("cloud4", imagesFolder + "cloud_4.png");
+            game.load.image("cloud5", imagesFolder + "cloud_5.png");
+            game.load.image("cloud6", imagesFolder + "cloud_6.png");
+            game.load.image("cloud7", imagesFolder + "cloud_7.png");
+            game.load.image("cloud8", imagesFolder + "cloud_8.png");
+            game.load.image("gift1", imagesFolder + "gift_1.png");
+            game.load.image("gift2", imagesFolder + "gift_2.png");
+            game.load.image("gift3", imagesFolder + "gift_3.png");
+            game.load.image("gift4", imagesFolder + "gift_4.png");
+            game.load.image("gift5", imagesFolder + "gift_5.png");
+            game.load.image("gift6", imagesFolder + "gift_6.png");
+            game.load.image("gift7", imagesFolder + "gift_7.png");
+            game.load.image("gift8", imagesFolder + "gift_8.png");
+            game.load.image("gift9", imagesFolder + "gift_9.png");
         },
         create: function()
         {
             this.pause = true;
             this.score = 0;
-
             this.sky = this.game.add.tileSprite(0, 0, this.game.cache.getImage("sky").width, this.game.height, "sky");
             
             this.city = this.game.add.tileSprite(0,
@@ -39,11 +48,12 @@
                 { font: "30px Arial", fill: "#ffffff" }); 
 
             this.obstacles = game.add.group();
+            this.obstacleTimer = game.time.events.loop(2000, this.addObstacle, this);
             this.clouds = game.add.group();   
             game.time.events.loop(1500, this.addCloud, this); 
+            this.gifts = game.add.group();  
+            this.giftsTimer = game.time.events.loop(1000, this.addGift, this);  
 
-            game.physics.startSystem(Phaser.Physics.ARCADE);
-            
             
 
             this.santa = game.add.sprite(100,245, "santa");
@@ -57,7 +67,7 @@
 
             var spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
             spaceKey.onDown.add(this.jump, this); 
-            this.timer = game.time.events.loop(1000, this.addObstacle, this);
+            
         },
         update: function()
         {
@@ -67,6 +77,7 @@
             if (this.santa.y < 0 || this.santa.y > screenHeight)
                 this.restartGame();
             game.physics.arcade.overlap(this.santa, this.obstacles, this.hitObstacle, null, this);
+            game.physics.arcade.overlap(this.santa, this.gifts, this.hitGift, null, this);
         },
         jump: function() 
         {
@@ -96,6 +107,29 @@
             cloud.checkWorldBounds = true;
             cloud.outOfBoundsKill = true;
         },
+        addGift: function()
+        {
+            if (this.pause) return;
+            let index = Math.floor(Math.random() * 9) + 1;
+            let x = screenWidth;
+            let y = Math.floor(Math.random() * screenHeight);
+            var gift = game.add.sprite(x, y, "gift" + index);
+            gift.width = 50;
+            gift.height = 50;
+            this.gifts.add(gift);
+            game.physics.arcade.enable(gift);
+            gift.body.velocity.x = -170;
+            gift.checkWorldBounds = true;
+            gift.outOfBoundsKill = true;
+        },
+        hitGift: function(santa, gift)
+        {
+            if (this.santa.alive == false)
+                return;
+            gift.kill();
+            this.score += 1;
+            this.labelScore.text = this.score;
+        },
         addObstacle: function()
         {
             if (this.pause) return;
@@ -110,33 +144,13 @@
             obstacle.checkWorldBounds = true;
             obstacle.outOfBoundsKill = true;
         },
-        addOneObstacle: function(x, y) 
-        {
-            var obstacle = game.add.sprite(x, y, 'obstacle');
-            obstacle.width = 50;
-            obstacle.height = 50;
-            this.obstacles.add(obstacle);
-            game.physics.arcade.enable(obstacle);
-            obstacle.body.velocity.x = -200; 
-            obstacle.checkWorldBounds = true;
-            obstacle.outOfBoundsKill = true;
-        },
-        addRowOfObstacles: function() 
-        {
-            if (this.pause) return;
-            var hole = Math.floor(Math.random() * 5) + 1;
-            for (var i = 0; i < rowsCount; i++)
-                if (i != hole && i != hole + 1) 
-                    this.addOneObstacle(400, i * 72);  
-            this.score += 1;
-            this.labelScore.text = this.score; 
-        },
         hitObstacle: function() 
         {
             if (this.santa.alive == false)
                 return;
             this.santa.alive = false;
-            game.time.events.remove(this.timer);
+            game.time.events.remove(this.obstacleTimer);
+            game.time.events.remove(this.giftsTimer);
             this.obstacles.forEach(function(p){
                 p.body.velocity.x = 0;
             }, this);
